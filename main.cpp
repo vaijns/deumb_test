37bbd7f92f4fc0d43c3a4ce17cf5cd910884a90f
vaijns <vaijnsxd@gmail.com>
2024-02-16 21:26:49 +0100

    replace static "check()"-call with static constexpr variable, so there's no extra parentheses needed

--- main.cpp
+++ main.cpp
@@ -4,13 +4,13 @@ int main(int argc, char* argv[]){
 	{
 		// runtime tests (print result)
 		using namespace dumb_test::operators;
-		dumb_test::evaluate<"aha">(dumb_test::check<"nop">(2, equals, 1));
+		static bool testResult = dumb_test::evaluate<"aha">(dumb_test::check<"nop">(2, equals, 1));
 	}
 
 	{
 		// static tests (fail at compile-time)
 		using namespace dumb_test::static_operators;
-		dumb_test::evaluate<"yep", dumb_test::check<"idk", 3, equals, 3>()>();
+		static constexpr bool testResult = dumb_test::static_evaluate<"yep", dumb_test::static_check<"idk", 3, equals, 3>>;
 	}
 
 	return 0;
